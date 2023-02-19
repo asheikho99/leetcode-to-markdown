2351d0717a1102868aff24490d9eea08de7d6316
asheikho <105573050+asheikho99@users.noreply.github.com>
2023-02-18 21:16:15 -0400

    Append problem details

--- script.py
+++ script.py
@@ -8,8 +8,21 @@ URL = input('Enter LeetCode URL\n')
 driver = webdriver.Chrome()
 driver.get(URL)
 time.sleep(3)
+
 problem_title = driver.find_element(By.XPATH, '//*[@id="qd-content"]/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[1]/div[1]/div/div/span').text
-time.sleep(3)
 f = open('problem.md', 'w')
-f.write('# ' + problem_title)
+f.write('#' + problem_title + '\n')
 f.close()
+
+time.sleep(3)
+
+header = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div[3]/div')
+children = header.find_elements(By.XPATH, './*')
+
+f = open('problem.md', 'a')
+for child in children:
+    print()
+    f.write(child.text)
+f.close()
+
+driver.close()
